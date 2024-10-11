6c05cc82d027305a2b72e5ac04fb980639c376ca
LONGDKZdev <longvo2549@gmail.com>
2024-10-11 15:29:52 +0700

    init

---
+++ NĂM 1/LTCB C++/Bài tập/Chapter4/Exercise1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+    int valueIn;
+    cout<<" Enter positive number = ";
+    cin>>valueIn;
+    if(valueIn >= 0){
+        cout<<valueIn<<" is positive number "<<endl;
+    }
+    else if(valueIn < 0)
+    cout<<valueIn<<" is not positive number "<<endl;
+
+    system("cls");
+}
--- NĂM 1/LTCB C++/IncludeAllLibrary.cpp
+++ NĂM 1/LTCB C++/IncludeAllLibrary.cpp
@@ -1,1 +1,11 @@
 #include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+    
+
+    
+
+}
+    // system("Pause");
+    // system("cls");
